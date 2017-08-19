fce2ed3640b06c94d93df215b38b94b257e9071a
James Nguyen <jamesnguyen.nyx@gmail.com>
2017-08-19 21:15:50 +0700

    comparing struct and class

--- ReferencesAndValues.playground/Contents.swift
+++ ReferencesAndValues.playground/Contents.swift
@@ -2,4 +2,65 @@
 
 import UIKit
 
+class Target {
+    var location = ""
+}
+
+var target = Target()
+target.location = "Death Star"
+
+var array = [1, 2, 3]
+var anotherArray = Array(1...3)
+
+//anotherArray.append(4)
+//print(anotherArray.count)
+
+
+if array == anotherArray {
+    print("Equal")
+}
+
+//Closure are references
+let printGreeting = {
+    print("Closure are reference type!")
+}
+
+printGreeting()
+let copyGreeting = printGreeting
+copyGreeting()
+
+func createIncrementer() -> () -> Void {
+    var counter = 0
+    
+    return {
+        counter += 1
+        print(counter)
+    }
+}
+
+let incrementer = createIncrementer()
+incrementer()
+incrementer()
+
+let incerementerCopy = incrementer
+incerementerCopy()
+incrementer()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
